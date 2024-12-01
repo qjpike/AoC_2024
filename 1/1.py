1a8f9b26d75da9e2b0f0778dd97f03d354e8d946
Quinten Pike <qpike@archer.com>
2024-12-01 00:27:14 -0800

    day 1

---
+++ 1/1.py
@@ -0,0 +1,35 @@
+
+
+def get_input():
+    inp = []
+    with open("input.txt") as f:
+        for i in f.readlines():
+            inp.append(i.strip())
+            
+    return inp
+
+
+def main():
+# Solution goes here
+    list_1 = []
+    list_2 = []
+
+    sum = 0
+    sum_2 = 0
+    for i in get_input():
+        list_1.append(int(i.strip().split()[0]))
+        list_2.append(int(i.strip().split()[1]))
+
+    for i in list_1:
+        sum_2 += i * list_2.count(i)
+    list_1.sort()
+    list_2.sort()
+
+    for i in range(len(list_1)):
+        sum += abs(list_1[i] - list_2[i])
+    print("Part 1:", sum)
+    print("Part 2:", sum_2)
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ template.py
@@ -0,0 +1,18 @@
+
+def get_input():
+    inp = []
+    with open("input.txt") as f:
+        for i in f.readlines():
+            inp.append(i.strip())
+            
+    return inp
+
+
+def main():
+# Solution goes here
+
+
+    return
+
+if __name__ == "__main__":
+    main()
